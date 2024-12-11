b58fa51be2120d0fe5918be8441c423af445707a
Theresia <theresiaoktavia2004@gmail.com>
2024-12-11 23:06:18 +0700

    revisi conflics

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/user/profile', [UserController::class, 'profile']);
 
 Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-// Route::get('/user/create', [UserController::class, 'create']);
-
 Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
-
-
-Route::get('/', [UserController::class, 'index'])->name('user.list');
-
-
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/list', [UserController::class, 'index'])->name('user.list');
+Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
 Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
 Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
-
 Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
-
-Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
